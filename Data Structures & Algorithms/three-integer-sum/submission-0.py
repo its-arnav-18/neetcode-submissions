933f0f1f0cb6e760fab581e090a457e2f234f423
its-arnav-18 <arnavmathur56@gmail.com>
2026-05-08 23:50:15 +0530

    Bulk sync: 27 submissions

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = set()
+        nums.sort()
+        for i in range(n):
+            for j in range(i+1,n):
+                for k in range(j+1,n):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        trp = [nums[i], nums[j], nums[k]]
+                        res.add(tuple(trp))
+        return [list(i) for i in res]
