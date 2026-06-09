b111378f1ea69524c065505317e091aa8df39d9c
Arnav Mathur <arnavmathur734@gmail.com>
2026-06-09 23:25:48 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        win = set()
+        for i in range(len(nums)):
+            if nums[i] in win:
+                return True
+            win.add(nums[i])
+
+            if len(win) > k:
+                win.remove(nums[i-k])
+        return False
+        
+
+        
